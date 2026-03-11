40f5e1845a3c9e70f470a4dc09b3450dfce63262
Srivikasini Velavan <srivikasinivelavan@gmail.com>
2026-03-11 20:53:43 +0530

    Added backend files and updates

--- backend/app/routes/resume_routes.py
+++ backend/app/routes/resume_routes.py
@@ -23,10 +23,24 @@ async def upload_resume(resume: UploadFile = File(...)):
     }
 """
 
-from fastapi import APIRouter
+"""from fastapi import APIRouter
 
 router = APIRouter()
 
 @router.post("/upload_resume")
 async def upload_resume():
-    return {"message": "Resume uploaded"}
+    return {"message": "Resume uploaded"}"""
+
+from fastapi import APIRouter, UploadFile, File
+
+router = APIRouter()
+
+@router.post("/upload_resume")
+async def upload_resume(file: UploadFile = File(...)):
+
+    print("Received file:", file.filename)
+
+    return {
+        "message": "Resume uploaded successfully",
+        "filename": file.filename
+    }
